11fdc4918abfddd07d5b88ef8d3f3a12b38f8c64
bezerkt1 <bezerkt1@hotmail.com>
2023-12-05 18:46:49 +0100

    Add score messages

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -14,6 +14,11 @@ export const Summary = () => {
         <h1>Summary</h1>
         <h2>
           You Scored: {score}% in a total time of {quiz.totalTime} seconds.
+          {score > 50
+            ? `Congratulations, you passed the test${
+                score > 90 ? " and not only that you aced it" : ""
+              }!`
+            : "Oh dear... sorry to say but you did not make it buddy. Better luck next time!"}
         </h2>
 
         <ol>
